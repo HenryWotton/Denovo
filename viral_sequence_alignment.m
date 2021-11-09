fa07ffd7e88ccfff76898c55633d5dbe89ef5f66
Geoffrey.Yu <geoffrey@Geoffreys-MBP.local>
2021-11-09 10:49:30 +0100

    modified viral headers

--- viral_sequence_alignment.m
+++ viral_sequence_alignment.m
@@ -3,23 +3,7 @@
 % ResultFile=['Distance' int2str(BLOUSUMno) '.mat'];
 % 
 % Prepare the main data file
-opts = detectImportOptions('9606_2021_cleaned');
-opts.Delimiter=",";
-data = readtable('9606_2021_cleaned',opts);
-ProteinA_Unique = unique(data.ProteinA);
-ProteinB_Unique = unique(data.ProteinB);
-ProteinA = data.ProteinA;
-ProteinB = data.ProteinB;
-GeneA = data.GeneA;
-GeneB = data.GeneB;
-SubfamilyA = data.FamilyA;
-SubfamilyB = data.FamilyB;
-Score = data.Score;
-PMID = data.PMID;
-Sub_Unique = unique(SubfamilyB);
-Tax_Unique = unique(data.TaxonB);
-TaxonA = unique(data.TaxonA);
-TaxonB = unique(data.TaxonB);
+
 BLOSUMno=30;
 ResultFile = ['Distance_' int2str(BLOSUMno) '_2021.mat'];
 [Headers, SeqV] = fastaread('all_viral_interacting_2021.fasta');
@@ -30,14 +14,32 @@ ResultFile = ['Distance_' int2str(BLOSUMno) '_2021.mat'];
 %     entry = test{2};
 %     check{a} = entry;
 % end
+% filter out the ones that do not appear in ProteinB_Unique
+true_member={};
+for a=1:1:length(Headers)
+    check = strsplit(Headers{a},"|");
+    entry = check{2};
+    if ismember(entry,ProteinB_Unique)
+        true_member{end+1} = Headers{a};
+    end
+end
 
+% store the sequences for true_member
+true_SeqV={};
+for i=1:length(Headers)
+    check = strsplit(Headers{i},"|");
+    entry = check{2};
+    if ismember(entry,ProteinB_Unique)
+        true_SeqV{end+1} = SeqV{i};
+    end
+end
 % global alignment 
-VpCount = length(SeqV);
+VpCount = length(true_SeqV);
 ScoringMatrix = ['BLOSUM' num2str(BLOSUMno)];
 ScoreVps = zeros(VpCount,VpCount);
 parfor a = 1:1:VpCount
     for b = 1:1:VpCount
-        ScoreVps(a,b) = nwalign(SeqV{1,a},SeqV{1,b},'ScoringMatrix',ScoringMatrix);
+        ScoreVps(a,b) = nwalign(true_SeqV{1,a},true_SeqV{1,b},'ScoringMatrix',ScoringMatrix);
     end
 end
 
@@ -47,9 +49,9 @@ colorbar;
 
 % having checked the plot, looks like 233 is an outlier 
 % below remove the outlier 
-ScoreVps(:,233) = 0;
-ScoreVps(233,:) = 0;
-
+% ScoreVps(:,233) = 0;
+% ScoreVps(233,:) = 0;
+% 
 Weights=zeros(VpCount,VpCount);
 for r=1:VpCount
     Row=ScoreVps(r,:);
@@ -57,4 +59,4 @@ for r=1:VpCount
     Weights(r,:)=1-((Row-min(Row))/(max(Row)-min(Row)));
 end
 
-save(ResultFile,'VpCount','BLOUSUMno','Weights')
+save(ResultFile,'VpCount','BLOSUMno','Weights','true_member','true_SeqV')
